47a2c9202989043e224a208a9fc65157099bbe8d
Zaki <muhammadfarid.zaki@gmail.com>
2026-02-16 16:12:54 +0700

    Fix: externalize solana modules for SSR

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,11 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [sveltekit()]
+  plugins: [sveltekit()],
+  ssr: {
+    external: ['@solana/web3.js', '@solana/pay']
+  },
+  optimizeDeps: {
+    exclude: ['@solana/web3.js', '@solana/pay']
+  }
 });
