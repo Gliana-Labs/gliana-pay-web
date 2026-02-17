68e46f87a4bc1a3fce6c822e94717a2d55c55f06
Zaki <muhammadfarid.zaki@gmail.com>
2026-02-17 15:00:37 +0700

    fix: replace floating emojis with SVG icons

--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -227,10 +227,18 @@
 <div class="min-h-screen bg-[#0a0a0b] text-white font-['Sora'] relative overflow-hidden">
   <!-- Floating crypto icons -->
   <div class="absolute inset-0 pointer-events-none overflow-hidden">
-    <div class="absolute top-20 left-[10%] text-4xl opacity-10 float">🪙</div>
-    <div class="absolute top-40 right-[15%] text-3xl opacity-10 float" style="animation-delay: 0.5s;">🚀</div>
-    <div class="absolute bottom-32 left-[20%] text-3xl opacity-10 float" style="animation-delay: 1s;">💎</div>
-    <div class="absolute bottom-20 right-[10%] text-4xl opacity-10 float" style="animation-delay: 1.5s;">⚡</div>
+    <div class="absolute top-20 left-[10%] w-16 h-16 opacity-10 float">
+      <svg class="w-full h-full text-purple-400" viewBox="0 0 24 24" fill="currentColor"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-1 17.93c-3.95-.49-7-3.85-7-7.93 0-.62.08-1.21.21-1.79L9 15v1c0 1.1.9 2 2 2v1.93zm6.9-2.54c-.26-.81-1-1.39-1.9-1.39h-1v-3c0-.55-.45-1-1-1H8v-2h2c.55 0 1-.45 1-1V7h2c1.1 0 2-.9 2-2v-.41c2.93 1.19 5 4.06 5 7.41 0 2.08-.8 3.97-2.1 5.39z"/></svg>
+    </div>
+    <div class="absolute top-40 right-[15%] w-14 h-14 opacity-10 float" style="animation-delay: 0.5s;">
+      <svg class="w-full h-full text-cyan-400" viewBox="0 0 24 24" fill="currentColor"><path d="M3.5 18.49l6-6.01 4 4L22 6.92l-1.41-1.41-7.09 7.97-4-4L2 16.99z"/></svg>
+    </div>
+    <div class="absolute bottom-32 left-[20%] w-14 h-14 opacity-10 float" style="animation-delay: 1s;">
+      <svg class="w-full h-full text-pink-400" viewBox="0 0 24 24" fill="currentColor"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 15l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z"/></svg>
+    </div>
+    <div class="absolute bottom-20 right-[10%] w-16 h-16 opacity-10 float" style="animation-delay: 1.5s;">
+      <svg class="w-full h-full text-yellow-400" viewBox="0 0 24 24" fill="currentColor"><path d="M7 2v11h3v9l7-12h-4l4-8z"/></svg>
+    </div>
   </div>
 
   <div class="absolute inset-0 overflow-hidden">
