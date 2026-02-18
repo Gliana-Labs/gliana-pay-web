3a746bd031d32b4227cdf24d24f2a56e4dbe7842
Zaki <muhammadfarid.zaki@gmail.com>
2026-02-19 05:39:20 +0700

    fix: use regular img tags instead of enhanced-img
    Copy PNGs to static folder for direct serving

--- src/lib/components/FloatingIcons.svelte
+++ src/lib/components/FloatingIcons.svelte
@@ -1,22 +1,17 @@
 <script lang="ts">
-  import dollarIcon from '$lib/assets/3dicons-dollar-dynamic-color.png?enhanced';
-  import walletIcon from '$lib/assets/3dicons-wallet-dynamic-color.png?enhanced';
-  import shieldIcon from '$lib/assets/3dicons-shield-dynamic-color.png?enhanced';
-  import videoCamIcon from '$lib/assets/3dicons-video-cam-dynamic-color.png?enhanced';
-
   const icons = [
-    { src: dollarIcon, alt: '', classes: 'top-[10%] left-[5%] w-16 h-16', delay: '0s' },
-    { src: walletIcon, alt: '', classes: 'top-[15%] right-[8%] w-16 h-16', delay: '1s' },
-    { src: shieldIcon, alt: '', classes: 'bottom-[20%] left-[10%] w-14 h-14', delay: '2s' },
-    { src: videoCamIcon, alt: '', classes: 'bottom-[15%] right-[5%] w-16 h-16', delay: '1.5s' }
+    { src: '/3dicons-dollar-dynamic-color.png', alt: '', classes: 'top-[10%] left-[5%] w-16 h-16', delay: '0s' },
+    { src: '/3dicons-wallet-dynamic-color.png', alt: '', classes: 'top-[15%] right-[8%] w-16 h-16', delay: '1s' },
+    { src: '/3dicons-shield-dynamic-color.png', alt: '', classes: 'bottom-[20%] left-[10%] w-14 h-14', delay: '2s' },
+    { src: '/3dicons-video-cam-dynamic-color.png', alt: '', classes: 'bottom-[15%] right-[5%] w-16 h-16', delay: '1.5s' }
   ];
 </script>
 
 {#each icons as icon}
-  <enhanced:img
+  <img
     src={icon.src}
     alt={icon.alt}
-    class="absolute pointer-events-none overflow-hidden {icon.classes} opacity-40 float"
+    class="absolute pointer-events-none overflow-hidden z-0 {icon.classes} opacity-40 float"
     style="animation-delay: {icon.delay};"
   />
 {/each}
