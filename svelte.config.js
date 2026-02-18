ea06af5b459a999d27bccaf72640c83c0a658e7a
Zaki <muhammadfarid.zaki@gmail.com>
2026-02-19 03:06:26 +0700

    feat: upgrade SvelteKit to v2.52.2 with @sveltejs/enhanced-img

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,11 @@
 import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { enhancedImages } from '@sveltejs/enhanced-img';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: vitePreprocess(),
+  plugins: [enhancedImages()],
   kit: {
     adapter: adapter()
   }
