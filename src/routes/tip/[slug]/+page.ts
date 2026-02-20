e5f62aeebcdd06d8a54f048cf98ee39386a102ab
Zaki <muhammadfarid.zaki@gmail.com>
2026-02-21 02:08:03 +0700

    Remove SSR disable, test with regular load

--- src/routes/tip/[slug]/+page.ts
+++ src/routes/tip/[slug]/+page.ts
@@ -1,8 +1,5 @@
 import type { PageLoad } from './$types';
 
-// Disable SSR for this page - data loads client-side
-export const ssr = false;
-
 export const load: PageLoad = ({ params }) => {
   return {
     slug: params.slug
