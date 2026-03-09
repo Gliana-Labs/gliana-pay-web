8e8222bace3249c2b20a4af5bcc7ceecca493d3a
Zaki <muhammadfarid.zaki@gmail.com>
2026-03-09 11:38:12 +0700

    chore: enable mainnet config

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -5,5 +5,8 @@ export const WORKER_URL_SERVER = ''; // relative URL (server-side)
 export const WORKER_HOST = ''; // for WebSocket
 
 // Solana Network configuration (Change these for mainnet later)
-export const SOLANA_RPC = 'https://api.devnet.solana.com'; // Mainnet: https://api.mainnet.solana.com
-export const USDC_MINT = '4zMMC9srt5Ri5X14GAgXhaHii3GnPAEERYPJgZJDncDU'; // Mainnet: EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v
+// export const SOLANA_RPC = 'https://api.devnet.solana.com';
+// export const USDC_MINT = '4zMMC9srt5Ri5X14GAgXhaHii3GnPAEERYPJgZJDncDU';
+//Mainnet
+export const SOLANA_RPC = 'https://api.mainnet.solana.com';
+export const USDC_MINT = 'EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v';
