c98cc2b8f6203a96a4dbb02cf5fa0491ab18b642
Zaki <muhammadfarid.zaki@gmail.com>
2026-03-11 12:17:28 +0700

    Update

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -5,8 +5,8 @@ export const WORKER_URL_SERVER = ''; // relative URL (server-side)
 export const WORKER_HOST = ''; // for WebSocket
 
 // Solana Network configuration (Change these for mainnet later)
-// export const SOLANA_RPC = 'https://api.devnet.solana.com';
-// export const USDC_MINT = '4zMMC9srt5Ri5X14GAgXhaHii3GnPAEERYPJgZJDncDU';
+export const SOLANA_RPC = 'https://api.devnet.solana.com';
+export const USDC_MINT = '4zMMC9srt5Ri5X14GAgXhaHii3GnPAEERYPJgZJDncDU';
 //Mainnet
-export const SOLANA_RPC = 'https://mainnet.helius-rpc.com/?api-key=b3a3b7bf-3794-4a88-8b64-258b79657c48';
-export const USDC_MINT = 'EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v';
+// export const SOLANA_RPC = 'https://mainnet.helius-rpc.com/?api-key=b3a3b7bf-3794-4a88-8b64-258b79657c48';
+// export const USDC_MINT = 'EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v';
